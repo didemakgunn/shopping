b67f6fcb49b2072ce425fbfe612c48eacee3f0aa
Didem Akgün <143537979+didemakgunn@users.noreply.github.com>
2025-03-06 16:28:43 +0300

    API’ye satın alma isteği

--- lib/cartProvider.dart
+++ lib/cartProvider.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'product.dart';
 
 class CartProvider with ChangeNotifier {
   final List<Product> _cartItems = [];
 
   List<Product> get cartItems => _cartItems;
+  double get totalPrice => _cartItems.fold(0, (sum, item) => sum + item.price);
 
   void addToCart(Product product) {
     _cartItems.add(product);
@@ -16,5 +19,35 @@ class CartProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  double get totalPrice => _cartItems.fold(0, (sum, item) => sum + item.price);
+  Future<bool> purchaseItems() async {
+    final url = Uri.parse('http://localhost:5001/purchase');
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode({
+          "cartItems": _cartItems
+              .map((product) => {
+                    "id": product.id,
+                    "name": product.name,
+                    "price": product.price
+                  })
+              .toList(),
+          "totalAmount": totalPrice,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        _cartItems.clear();
+        notifyListeners();
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      print("Hata: $e");
+      return false;
+    }
+  }
 }
